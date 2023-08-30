d50e99f183107442911004edb0c606f49e1eb5f1
viventer <viventermail@gmail.com>
2023-08-30 20:19:30 +0200

    zamiana zadania

--- rozwiazanie.py
+++ rozwiazanie.py
@@ -1,38 +1,57 @@
-def format_user_info(user_data):
-    # Zamieniamy stringa na listę, elementy rozdzielone są spacjami
-    user_data = user_data.split(" ")
-    # Z utworzonej listy pobieramy dane rozpakowując listę
-    first_name, last_name, age, email = user_data
+# Główna funkcja formatująca informacje o poście
+def format_post(post_info):
+    # Dzielę stringa w miejscach spacji
+    parts = post_info.split()
+    # Jego pierwsza część to nazwa użytkownika
+    username = parts[0]
+    # Tytuł to wszystkie części od pierwszej do przedostatniej
+    title = ' '.join(parts[1:-1])
+    # A data stworzenia to ostatnia cześć
+    creation_date = format_date(parts[-1])
+
+    formatted_post_info = f"""autor: {username}
+tytuł: {title}
+data utworzenia: {creation_date}"""
+
+    return formatted_post_info
+
+
+# Funkcja pomocnicza formatująca datę
+def format_date(date_str):
     """
-    Rozpakowując listę wszystkie zmienne tworzymy naraz przypisując do nich 
-    kolejne elementy, ale oczywiście możemy to też zrobić klasyczną metodą:
-    first_name = user_data[0]
-    last_name = user_data[1]
-    age = user_data[2]
-    email = user_data[3]
+    Do oddzielenia liczb skorzystałem z funkcji split, a oprócz tego używałem odpakowania 
+    listy, dzięki czemu wszystkie zmienne (day, month, year) tworzę jednocześnie z kolejnych 
+    elementów listy.
+    Można to też zrobić w klasyczny sposób:
+    date_list = date_str.split(".")
+    day = date_list[0]
+    month = date_list[1]
+    year = date_list[2]
     """
+    day, month, year = date_str.split('.')
+    # Stworzyłem słownik dzięki któremu zamienię miesiące podane liczbowo na ich nazwy
+    months = {
+        '01': 'stycznia', '02': 'lutego', '03': 'marca', '04': 'kwietnia',
+        '05': 'maja', '06': 'czerwca', '07': 'lipca', '08': 'sierpnia',
+        '09': 'września', '10': 'października', '11': 'listopada', '12': 'grudnia'
+    }
+    # Jeśli dzień rozpoczyna się od 0 - usuwam je
+    day = day.removeprefix("0")
+    formatted_date = f"{day} {months[month]} 20{year} r."
+    return formatted_date
 
-    # Teraz najważniejsze - tworzymy sformatowane dane za pomocą f-stringa:
-    formatted_info = f"""imie: {first_name}
-nazwisko: {last_name}
-wiek: {age}
-pełnoletni: {"tak" if int(age) >= 18 else "nie"}
-email: {email}"""
 
-    return formatted_info
+# GOTOWA CZĘŚĆ KODU ---------------------------------------------------------
+input_filename = 'posts.txt'
+output_filename = 'properly_formatted_posts.txt'
 
+with open(input_filename, 'r') as input_file:
+    posts_info = input_file.readlines()
 
-# Gotowa część ---------------------------
-file_name = "users.txt"
+formatted_posts_info = []
+for post_line in posts_info:
+    formatted_post_info = format_post(post_line)
+    formatted_posts_info.append(formatted_post_info + "\n\n")
 
-try:
-    with open(file_name, "r") as file:
-        for line in file:
-            print(f"{'-' * 30}")
-            formatted_info = format_user_info(line)
-            print(formatted_info, end="")
-        print(f"\n{'-' * 30}")
-except FileNotFoundError:
-    print(f'Nie znaleziono pliku: {file_name}.')
-except:
-    print(f"Błąd odczytu pliku: {file_name}.")
+with open(output_filename, 'w') as output_file:
+    output_file.writelines(formatted_posts_info)
--- twoje_rozwiazanie.py
+++ twoje_rozwiazanie.py
@@ -1,17 +1,45 @@
-def format_user_info(user_data):
-    # Wypełnij funkcję
-    return formatted_info
+# ROZWIĄZANIE ---------------------------------------------------------
+
+def format_post(post_info):
+    # Tutaj uzupełnij funkcje
+
+    return formatted_post_info
 
 
-# Gotowa część ---------------------------
-file_name = "users.txt"
+# GOTOWA CZĘŚĆ KODU ---------------------------------------------------------
 
-try:
-    with open(file_name, "r") as file:
-        for line in file:
-            print(f"{'-' * 30}")
-            formatted_info = format_user_info(line)
-            print(formatted_info, end="")
-        print(f"\n{'-' * 30}")
-except FileNotFoundError:
-    print(f'Nie znaleziono pliku: {file_name}.')
+input_filename = 'posts.txt'
+output_filename = 'formatted_posts.txt'
+
+with open(input_filename, 'r') as input_file:
+    posts_info = input_file.readlines()
+
+formatted_posts_info = []
+for post_line in posts_info:
+    formatted_post_info = format_post(post_line)
+    formatted_posts_info.append(formatted_post_info + "\n\n")
+
+with open(output_filename, 'w') as output_file:
+    output_file.writelines(formatted_posts_info)
+
+
+# SPRAWDZENIE CZY KOD DZIAŁA PRAWIDŁOWO --------------------------------------
+
+checker_filename = "properly_formatted_posts.txt"
+
+with open(checker_filename, "r") as checker_file:
+    properly_formatted_posts_info = checker_file.readlines()
+
+with open(output_filename, "r") as output_file:
+    formatted_posts_info = output_file.readlines()
+
+is_correct = "".join(formatted_posts_info) == "".join(
+    properly_formatted_posts_info)
+
+if is_correct:
+    print("Gratulacje! Twoja funkcja zadziałała prawidłowo.")
+else:
+    print(f"""Niestety twoja funkcja nie działa prawidłowo.
+Sprawdź swój plik wyjściowy - {output_filename} 
+i porównaj go z prawidłowym plikiem - {checker_filename}
+""")
